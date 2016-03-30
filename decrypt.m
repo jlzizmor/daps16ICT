2e3df80423c612c29287189e44b605b53c9fb6b6
Jacob Zizmor <jlzizmor@wpi.edu>
2016-03-29 22:16:21 -0400

    added more description to the function comments

--- decrypt.m
+++ decrypt.m
@@ -11,11 +11,22 @@
 %% Function
 % This function is used to analyze the input stream from an asynchronous
 % serial object.
+% Single byte (8 bits) are read from the Serial object
+% byte is analyzed as such:
+%     [7]-[3] = ignored
+%     [2] = to active
+%     [1] = at_pos
+%     [0] = detected
+% Function only runs when new data is read in Serial buffer
 % INPUT = asynchronous Serial object
-% OUTPUT = [boolean boolean boolean]
-% to_active is true when the LA are moving towards active pressure
-% at_pos is true when the LA have reached their goal position
-% detected is true when the EMG algorithm has confirmed movement
+% OUTPUT = [int int int]
+% to_active is 1 when the LA are moving towards active pressure, 0
+% if not
+% at_pos is 1 when the LA have reached their goal position, 0
+% if not
+% detected is 1 when the EMG algorithm has confirmed movement, 0
+% if not
+% all variables returned will be -1 if no information is in Serial buffer
 
 function [ to_active, at_pos, detected ] = decrypt( ~ )
 
