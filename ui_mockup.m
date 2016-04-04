6b80c8966268081a24a825bdb22b92084b611388
Jacob Zizmor <jlzizmor@wpi.edu>
2016-04-04 00:52:17 -0400

    added plot for pressure, fixed axes, made the layout more dynamic

--- ui_mockup.m
+++ ui_mockup.m
@@ -1,15 +1,21 @@
 close all
+num_rows = 20;
 
 figure % http://www.mathworks.com/help/matlab/examples/displaying-multiple-plots-in-a-single-figure.html
 
-subplot(3,3,[2 3 5 6 8 9])
+subplot(num_rows, num_rows, [(num_rows-(0.6*num_rows)) (num_rows*num_rows)])
 t = 0:pi/50:10*pi;
 st = sin(t);
 ct = cos(t);
 plot3(st,ct,t) % http://www.mathworks.com/help/matlab/ref/plot3.html
 title('Model')
+ax = gca; % http://www.mathworks.com/help/matlab/ref/gca.html
+ax.Box = 'on';
+set(gca, 'XTick', []);
+set(gca, 'YTick', []);
+set(gca, 'ZTick', []);
 
-subplot(3,3,1) 
+subplot(num_rows,num_rows,[(0.25*num_rows) (2+num_rows)] )
 E = 0;
 N = 0;
 r=1;
@@ -21,3 +27,11 @@ ax = gca; % http://www.mathworks.com/help/matlab/ref/gca.html
 ax.Color = 'none'; % http://www.mathworks.com/help/matlab/ref/axes-properties.html
 ax.XColor = 'none';
 ax.YColor = 'none';
+
+subplot(num_rows, num_rows, [ (1 + 4*num_rows) ((num_rows*num_rows)-(0.7*num_rows)) ])
+x = 0:pi/100:2*pi;
+y = sin(x);
+plot(x,y)
+ax = gca; % http://www.mathworks.com/help/matlab/ref/gca.html
+ax.XColor = 'none';
+ylim([0 2.5])
