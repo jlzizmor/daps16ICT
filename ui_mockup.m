a792bfa4e1e4b9a7c99b03688caafaba1dd10300
Jacob Zizmor <jlzizmor@wpi.edu>
2016-04-04 00:28:40 -0400

    modifications to circle's axes

--- ui_mockup.m
+++ ui_mockup.m
@@ -1,3 +1,5 @@
+close all
+
 figure % http://www.mathworks.com/help/matlab/examples/displaying-multiple-plots-in-a-single-figure.html
 
 subplot(3,3,[2 3 5 6 8 9])
@@ -10,8 +12,11 @@ title('Model')
 subplot(3,3,1) 
 E = 0;
 N = 0;
-r=1; % some radius
-color=[0 1 0]; % red color
+r=1;
+color=[0 1 0];
 t=linspace(0,2*pi);
 fill(E+r*cos(t),N+r*sin(t),color); % this creates a filled circle centered at (E,N)
 axis square
+ax = gca; % http://www.mathworks.com/help/matlab/ref/gca.html
+ax.Color = 'none';
+ax.Box = 'off';
